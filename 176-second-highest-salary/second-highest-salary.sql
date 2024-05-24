1a29c6b5a8826320b5e091f26ce8872a16fe53f8
deepak <37184414+dk46164@users.noreply.github.com>
2024-05-24 23:35:30 +0530

    Time: 1242 ms (18.99%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,3 +1,5 @@
-SELECT MAX(SALARY) AS SecondHighestSalary
- FROM EMPLOYEE
- WHERE SALARY <> (SELECT MAX(SALARY) FROM EMPLOYEE);
+WITH CTE AS
+(
+Select id, Salary,DENSE_RANK() OVER	(ORDER BY salary DESC) Rn FROM Employee
+)
+SELECT MAX(Salary) AS SecondHighestSalary FROM CTE WHERE rn = 2;
