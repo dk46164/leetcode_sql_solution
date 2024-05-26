a7c25f415973fb10865bb9d4de60c651fb598ec2
deepak <37184414+dk46164@users.noreply.github.com>
2024-05-27 01:18:15 +0530

    Time: 1360 ms (29.29%) | Memory: 0B (100.00%) - LeetSync

---
+++ 185-department-top-three-salaries/department-top-three-salaries.sql
@@ -0,0 +1,8 @@
+/* Write your T-SQL query statement below */
+select department , employee , salary 
+from (
+  select e.name employee , e.salary salary, d.name department, dense_rank() over (partition by d.id order by e.salary desc) ranking
+  from employee e inner join Department d
+  on e.departmentId = d.id
+) newTable
+where ranking <= 3;
