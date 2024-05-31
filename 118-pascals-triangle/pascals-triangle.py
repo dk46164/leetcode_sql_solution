39dba05aac3a73e4ab93feecaaf4e756a0ed1d2f
deepak <37184414+dk46164@users.noreply.github.com>
2024-05-31 22:21:46 +0530

    Time: 34 ms (76.14%) | Memory: 16.5 MB (53.05%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.py
+++ 118-pascals-triangle/pascals-triangle.py
@@ -1,24 +1,15 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
         
-        if numRows==1:
-            return [[1]]
-        elif numRows==2:
-            return [[1],[1,1]]
-        else:
-            # intialized the results
-            pascl_rows = [[1],[1,1]]
-
-            # loop through each (n-2)
-            for idx in range(numRows-2):
-                prev_res = []
-                prev_val = 0
-                for val in pascl_rows[-1]:
-                    prev_res.append(prev_val+val)
-                    prev_val=val
-                prev_res.append(1)
-                pascl_rows.append(prev_res)
-            return pascl_rows
+        rows = [[1], [1, 1]]
+        if numRows <= 2:
+            return rows[:numRows]
+        for i in range(3, numRows+1):
+            new_row = [x[0]+x[1] for x in itertools.pairwise(rows[-1])]
+            new_row.insert(0,1)
+            new_row.append(1)
+            rows.append(new_row)
+        return rows
 
 
 
